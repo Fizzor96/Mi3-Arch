36e7563f26738f2945971cf4677552d0531284d6
Fizzor96 <marwel96@gmail.com>
2022-10-14 19:53:42 +0200

    bugfixes

--- installscript.sh
+++ installscript.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 
 # update sys
-yay --noconfirm --answerdiff None --answerclean None --removemake -Syy
-yay --noconfirm --answerdiff None --answerclean None --removemake -Syu
+pacman --noconfirm -Syyu
 
 # prereq
 sudo pacman --noconfirm -S --needed base-devel
-sudo pacman --noconfirm -S git wget sudo nano neovim ranger python python-pip perl
+sudo pacman --noconfirm -S --needed git wget sudo nano neovim ranger python python-pip perl
 
 # installing yay
 sudo git clone https://aur.archlinux.org/yay.git
@@ -18,20 +17,28 @@ sudo rm -rf yay
 
 # install dependencies
 # spotify cuz i3blocks script require a spotify install 
-yay --noconfirm --answerdiff None --answerclean None --removemake -Sy i3exit ttf-yosemite-san-francisco-font-git ttf-fira-code ttf-font-awesome ttf-inconsolata lxappearance moka-icon-theme-git arc-gtk-theme-git capitaine-cursors picom spotify rxvt-unicode
+yay --noconfirm --answerdiff None --answerclean None --removemake -Sy --needed i3exit ttf-yosemite-san-francisco-font-git ttf-fira-code ttf-font-awesome ttf-inconsolata lxappearance moka-icon-theme-git arc-gtk-theme-git capitaine-cursors picom spotify rxvt-unicode visual-studio-code-bin
 # neovide??
 pip install dbus-python
 
 # copy file
 cd dots
 cp -r .config -t ~/
-cp .bashrc .bash_profile .gtkrc-2.0 .Xresources
+cp .bashrc .bash_profile .gtkrc-2.0 .Xresources -t ~/
 # chmod +x scripts...
 chmod +x ~/.config/i3blocks/scripts
 cd ..
 
+# apply changes
+i3-msg restart
+xrdb ~/.Xresources
+
 # Set picom config
 picom --config ~/.config/picom/picom.conf
+
+# prevent packman/yay to update/upgrade "linux", pacmanconf addition
+# pacman config file location: /etc/pacman.conf
+sed -i 's/#IgnorePkg   =/IgnorePkg   = linux/g' /etc/pacman.conf
 
 # Cleanup
 yay -Yc --noconfirm
