4a747f1d601b7a80f499dbdac8c5fcee6f698d1f
Fizzor96 <marwel96@gmail.com>
2022-10-16 22:35:31 +0200

    typo fixes

--- installscript.sh
+++ installscript.sh
@@ -5,7 +5,7 @@ pacman --noconfirm -Syyu
 
 # prereq
 sudo pacman --noconfirm -S --needed base-devel
-sudo pacman --noconfirm -S --needed git wget sudo nano neovim ranger python python-pip perl
+sudo pacman --noconfirm -S --needed git wget sudo nano ranger python python-pip perl
 
 # installing yay
 sudo git clone https://aur.archlinux.org/yay.git
@@ -17,15 +17,14 @@ sudo rm -rf yay
 
 # install dependencies
 # spotify cuz i3blocks script require a spotify install 
-yay --noconfirm --answerdiff None --answerclean None --removemake -Sy --needed i3exit ntfs-3g ttf-yosemite-san-francisco-font-git ttf-fira-code ttf-font-awesome ttf-inconsolata lxappearance moka-icon-theme-git arc-gtk-theme-git capitaine-cursors picom i3blocks-contib acpi xorg-xbacklight xorg-xset alsa-utils acpilight network-manager-applet spotify rxvt-unicode visual-studio-code-bin
+yay --noconfirm --answerdiff None --answerclean None --removemake -Sy --needed feh i3exit ntfs-3g ttf-yosemite-san-francisco-font-git ttf-fira-code ttf-font-awesome ttf-inconsolata lxappearance moka-icon-theme-git arc-gtk-theme-git capitaine-cursors picom i3blocks-contrib acpi xorg-xset alsa-utils network-manager-applet spotify rxvt-unicode visual-studio-code-bin
 
 pip install dbus-python
 
-# copy file
+# copy configs
 cd dots
 cp -r .config -t ~/
-cp .bashrc .bash_profile .gtkrc-2.0 .Xresources -t ~/
-# chmod +x scripts...
+cp .bashrc .wallpaper.png .bash_profile .gtkrc-2.0 .Xresources -t ~/
 chmod +x ~/.config/i3blocks/scripts
 cd ..
 
@@ -38,16 +37,10 @@ picom --config ~/.config/picom/picom.conf
 
 # prevent packman/yay to update/upgrade "linux", pacmanconf addition
 # pacman config file location: /etc/pacman.conf
-sed -i 's/#IgnorePkg   =/IgnorePkg   = linux/g' /etc/pacman.conf
+sudo sed -i 's/#IgnorePkg   =/IgnorePkg   = linux/g' /etc/pacman.conf
 
 # brightnesscontrolls 
 sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
-
-# audiocontrolls
-systemctl --system enable --now pulseaudio.service
-# systemctl --user enable pulseaudio.service/socket
-systemctl --system enable pulseaudio.service/socket
-systemctl --system start pulseaudio.service
 
 # network
 systemctl start NetworkManager
@@ -63,8 +56,8 @@ systemctl enable NetworkManager
 # sudo sed -i 's/# display-setup-script = Script to run when starting a greeter session (runs as root)/display-setup-script = /etc/lightdm/lightdmxrandr.sh/g' /etc/lightdm/lightdm.conf
 
 
-i3-msg restart
+#i3-msg restart
 # Cleanup
 yay -Yc --noconfirm
 
-sudo reboot now
+#sudo reboot now
