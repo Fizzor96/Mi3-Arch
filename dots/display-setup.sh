f3fd21f25aa41834d78e3c246239622dfa0a1b70
Fizzor96 <marwel96@gmail.com>
2022-10-20 11:22:25 +0200

    lightdm-webkit2 w theme install

---
+++ dots/display-setup.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,18 @@
 #!/usr/bin/bash
 
-# update sys
+# Update system
 pacman --noconfirm -Syyu
 
-# prereq
+# Ranking best mirrors for Arch
+pacman -S pacman-contrib --noconfirm
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+# Prerequisites
 sudo pacman --noconfirm -S --needed base-devel
 sudo pacman --noconfirm -S --needed git wget sudo nano ranger python python-pip perl
 
-# installing yay
+# Install yay
 yay_install_path=/usr/bin/yay
 if [ -f "$yay_install_path" ]
 then
@@ -21,43 +26,67 @@ else
     sudo rm -rf yay
 fi
 
+# Install required packages
+PKGS=(
+'lightdm-webkit2-greeter'
+'geany'
+'geany-themes'
+'feh'
+'i3exit'
+'i3lock' 
+'ntfs-3g'
+'ttf-yosemite-san-francisco-font-git'
+'ttf-fira-code ttf-font-awesome'
+'ttf-inconsolata'
+'lxappearance'
+'moka-icon-theme-git'
+'arc-gtk-theme-git'
+'capitaine-cursors'
+'i3blocks-contrib'
+'acpi'
+'xorg-xset'
+'alsa-utils'
+'iwgtk'
+'spotify-launcher'
+'rxvt-unicode'
+'dbus'
+'picom'
+'visual-studio-code-bin'
+'chromium'
+)
 
-# install dependencies
-# spotify cuz i3blocks script require a spotify install 
-yay --noconfirm --answerdiff None --answerclean None --removemake -Sy --needed feh i3exit i3lock ntfs-3g ttf-yosemite-san-francisco-font-git ttf-fira-code ttf-font-awesome ttf-inconsolata lxappearance moka-icon-theme-git arc-gtk-theme-git capitaine-cursors i3blocks-contrib acpi xorg-xset alsa-utils iwgtk spotify-launcher rxvt-unicode dbus picom visual-studio-code-bin chromium
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed --answerdiff None --answerclean None
+done
 
 # for i3blocks-spotify
 pip install dbus-python
 
-# copy configs
+# Copy configs
 cd dots
 cp -r .config -t ~/
 cp .bashrc .wallpaper.png .bash_profile .gtkrc-2.0 .Xresources -t ~/
 chmod +x ~/.config/i3blocks/scripts
 cd ..
 
+# Copy lightdm theme
+git clone https://github.com/dimaglushkov/lightdm-webkit2-theme-minimal.git
+sudo cp -r lightdm-webkit2-theme-minimal -t /usr/share/lightdm-webkit/themes
+cd /usr/share/lightdm-webkit/themes
+sudo mv lightdm-webkit2-theme-minimal minimal
+
+# Prevent packman/yay to update/upgrade "linux", pacmanconf addition
+# pacman config file location: /etc/pacman.conf
+sudo sed -i 's/#IgnorePkg   =/IgnorePkg   = linux/g' /etc/pacman.conf
+
+# for brightnesscontroll
+# sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
+sudo usermod -aG video $USER
+
 # apply changes
 i3-msg restart
 xrdb ~/.Xresources
 
-# prevent packman/yay to update/upgrade "linux", pacmanconf addition
-# pacman config file location: /etc/pacman.conf
-sudo sed -i 's/#IgnorePkg   =/IgnorePkg   = linux/g' /etc/pacman.conf
-
-# brightnesscontrolls 
-# sudo chmod a+rw /sys/class/backlight/intel_backlight/brightness
-sudo usermod -aG video $USER
-
-# network
-# systemctl start NetworkManager
-# systemctl enable NetworkManager
-
-# lightdm resolution
-# sudo cp lightdmxrandr.sh -t /etc/lightdm/
-# chmod a+xr /etc/lightdm/lightdmxrandr.sh
-# sudo sed -i 's/# display-setup-script = Script to run when starting a greeter session (runs as root)/display-setup-script = /etc/lightdm/lightdmxrandr.sh/g' /etc/lightdm/lightdm.conf
-
 # Cleanup
 yay -Yc --noconfirm
-
-#sudo reboot now
